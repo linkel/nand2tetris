604b0eb6f3bf48c47afc7d48834a856a9d102dee
linkel <ironcarbide@gmail.com>
2020-04-02 15:52:06 -0700

    Create function Run to avoid duplication. Format code with string literal. Consider changing this, looks kinda ugly. Seems to work for SimpleAdd.vm.

--- 07/VMtranslator.py
+++ 07/VMtranslator.py
@@ -46,10 +46,14 @@ class Parser:
 
     def arg1(self):
         """Returns the first argument of the current command"""
-        return self.split_line[1]
+        return self.split_line[0]
 
     def arg2(self):
         """Returns the second argument of the current command"""
+        return self.split_line[1]
+
+    def arg3(self):
+        """Returns the third argument of the current command"""
         return self.split_line[2]
 
 
@@ -65,155 +69,177 @@ class CodeWriter:
     def write_arithmetic(self, command: str):
         """Writes the assembly code that is the translation of the arithmetic command."""
         if command == "add":
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                M=D+M
-                                @SP
-                                M=M+1''')
+            self.file.write('''
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=D+M
+@SP
+M=M+1
+''')
         if command == "sub":
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                M=M-D
-                                @SP
-                                M=M+1''')
+            self.file.write('''
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+''')
         if command == "neg":
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                M=-M
-                                @SP
-                                M=M+1''')
+            self.file.write('''
+@SP
+M=M-1
+A=M
+M=-M
+@SP
+M=M+1
+''')
         if command == 'eq':
             label_eq = self._generate_label()
             label_neq = self._generate_label()
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                D=M-D
-                                @{}
-                                D;JEQ
-                                @SP
-                                A=M
-                                M=0
-                                @{}
-                                D;JNE
-                                ({})
-                                @SP
-                                A=M
-                                M=-1
-                                ({})
-                                @SP
-                                M=M+1
+            self.file.write('''
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
+@{}
+D;JEQ
+@SP
+A=M
+M=0
+@{}
+D;JNE
+({})
+@SP
+A=M
+M=-1
+({})
+@SP
+M=M+1
             '''.format(label_eq, label_neq, label_eq, label_neq))
         if command == 'gt':
             label_gt = self._generate_label()
             label_ngt = self._generate_label()
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                D=M-D
-                                @{}
-                                D;JGT
-                                @SP
-                                A=M
-                                M=0
-                                @{}
-                                D;JLE
-                                ({})
-                                @SP
-                                A=M
-                                M=-1
-                                ({})
-                                @SP
-                                M=M+1
+            self.file.write('''
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
+@{}
+D;JGT
+@SP
+A=M
+M=0
+@{}
+D;JLE
+({})
+@SP
+A=M
+M=-1
+({})
+@SP
+M=M+1
             '''.format(label_gt, label_ngt, label_gt, label_ngt))
         if command == 'lt':
             label_lt = self._generate_label()
             label_nlt = self._generate_label()
             self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                D=M-D
-                                @{}
-                                D;JLT
-                                @SP
-                                A=M
-                                M=0
-                                @{}
-                                D;JGE
-                                ({})
-                                @SP
-                                A=M
-                                M=-1
-                                ({})
-                                @SP
-                                M=M+1
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=M-D
+@{}
+D;JLT
+@SP
+A=M
+M=0
+@{}
+D;JGE
+({})
+@SP
+A=M
+M=-1
+({})
+@SP
+M=M+1
             '''.format(label_lt, label_nlt, label_lt, label_nlt))
         if command == "and":
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                M=D&M
-                                @SP
-                                M=M+1''')
+            self.file.write('''
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=D&M
+@SP
+M=M+1
+                                ''')
         if command == "or":
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                D=M
-                                @SP
-                                M=M-1
-                                A=M
-                                M=D|M
-                                @SP
-                                M=M+1''')
+            self.file.write('''
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=D|M
+@SP
+M=M+1
+                                ''')
         if command == "not":
-            self.file.write('''@SP
-                                M=M-1
-                                A=M
-                                M=!M
-                                @SP
-                                M=M+1''')
+            self.file.write('''
+@SP
+M=M-1
+A=M
+M=!M
+@SP
+M=M+1
+                                ''')
         else:
             return NotImplemented
 
     def write_pushpop(self, command: str, segment: str, index: int):
         """Writes the assembly code that is the translation of the given command push or pop."""
-        if command == "push":
+        if command == "C_PUSH":
             # do push stuff
             if segment == "constant":
-                self.file.write('@{}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1'.format(index))
+                self.file.write('''
+@{}
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+                                   '''.format(index))
             elif segment == "local":
                 pass
                 # Todo: implement the other push segments
         elif command == "C_POP":
-            # do pop stuff
+            # Todo: implement pop
             pass
         else:
             raise ValueError
@@ -225,7 +251,24 @@ class CodeWriter:
     def _generate_label(self) -> str:
         label_number = self.label_number
         self.label_number += 1
-        return 'label' + label_number
+        return 'label' + str(label_number)
+
+
+def run(f, filename):
+    lines = f.readlines()
+    lines = [line.partition('//')[0].strip() for line in lines]
+    lines = [line for line in lines if line]
+    parser = Parser(lines)
+    code_writer = CodeWriter()
+    code_writer.set_filename(os.path.splitext(filename)[0] + '.asm')
+    while True:
+        if parser.command_type() == 'C_ARITHMETIC':
+            code_writer.write_arithmetic(parser.arg1())
+        elif parser.command_type() == 'C_PUSH' or parser.command_type() == 'C_POP':
+            code_writer.write_pushpop(parser.command_type(), parser.arg2(), parser.arg3())
+        if not parser.has_more_commands():
+            break
+        parser.advance()
 
 
 if __name__ == "__main__":
@@ -233,23 +276,12 @@ if __name__ == "__main__":
         print("usage: VMtranslator.py source")
     path = sys.argv[1]
     if os.path.isdir(path):
-        # do directory stuff (for each .vm file in there, run the parser thing on it)
         for directory, subdirectories, files in os.walk(path):
             for file in files:
                 if file.endswith('.vm'):
-                    with open(file) as f:
-                        lines = f.readlines()
-                        lines = [line.partition('//')[0].strip() for line in lines]
-                        lines = [line for line in lines if line]
-                        parser = Parser(lines)
-                        code_writer = CodeWriter()
-                        code_writer.set_filename(os.path.splitex(file)[0] + '.asm')
+                    with open(os.path.join(path, file)) as f:
+                        run(f, file)
 
     elif os.path.isfile(path) and os.path.splitext(path)[1] == '.vm':
         with open(path) as f:
-            lines = f.readlines()
-            lines = [line.partition('//')[0].strip() for line in lines]
-            lines = [line for line in lines if line]
-            parser = Parser(lines)
-            code_writer = CodeWriter()
-            code_writer.set_filename(os.path.splitex(path)[0] + '.asm')
+            run(f, path)
