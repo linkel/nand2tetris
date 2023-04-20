be00522b29be3948f926d30f595ed3d39ec1fa4b
linkel <ironcarbide@gmail.com>
2023-04-19 22:57:24 -0700

    It begins! Actual compilation code attempts, not yet tested, need to add in the xml writing after I write 'em all out. Here we go recursive descent parser LL(1)

--- 10/CompilationEngine.py
+++ 10/CompilationEngine.py
@@ -1,6 +1,14 @@
 import xml.etree.ElementTree as ET
 from JackTokenizer import JackTokenizer, TokenType
 
+statement_keywords = {
+    'let',
+    'do',
+    'if',
+    'while',
+    'return'
+}
+
 class CompilationEngine:
     def __init__(self, tokenizer: JackTokenizer, outputFilename):
         self.tree = None
@@ -65,29 +73,95 @@ class CompilationEngine:
         self.tree = root
         self._expect("class")
         self._expect("", True) # lol think of a better way to permit identifiers
+        self._expect("{")
+        while (self.tokenizer.token_type() == TokenType.keyword and (self.tokenizer.keyword() == "static" or self.tokenizer.keyword() == "field")):
+            self.compile_class_var_dec()
+        
+        while (self.tokenizer.token_type() == TokenType.keyword and (self.tokenizer.keyword() == "constructor" or self.tokenizer.keyword() == "method" or self.tokenizer.keyword() == "function")):
+            self.compile_subroutine()
+        self._expect("}")
+
 
     '''Compiles a static declaration or a field declaration.'''
     def compile_class_var_dec(self):
-        NotImplemented
+        if self._accept("static") or self._accept("field"):
+            self.compile_type()
+            self._expect("", True) # varName
+            while self._accept(","):
+                self._expect("", True)
+            self._expect(";")
+
 
     '''Compiles a complete method, function, or constructor'''
     def compile_subroutine(self):
-        NotImplemented
+        if self._accept("constructor") or self._accept("method") or self._accept("function"):
+            if not self._accept("void"):
+                self.compile_type()
+            self._expect("", True) # subroutineName
+            self._expect("(")
+            self.compile_parameter_list()
+            self._expect(")")
+
+            # subroutine body
+            self._expect("{")
+            while (self.tokenizer.token_type() == TokenType.keyword and (self.tokenizer.keyword() == "var")):
+                self.compile_var_dec()
+            self.compile_statements()
+            self._expect("}")
+
+    '''Compiles a type, I added this contract for my own ease'''
+    def compile_type(self, optional = False):
+        if not (self._accept("int") or self._accept("char") or self._accept("boolean")):
+            if optional:
+                self._accept("", True)
+            else:
+                self._expect("", True)
 
     '''Compiles a parameter list, not including the enclosing ()'''
     def compile_parameter_list(self):
-        NotImplemented
+        if self.compile_type(True): # bool to show it's optional
+            self._expect("", True)
+            while self._accept(","):
+                self.compile_type()
+                self._expect("", True)
+        # else it's an empty param list
 
     '''Compiles a var declaration'''
     def compile_var_dec(self):
-        NotImplemented
+        self._expect("var")
+        self.compile_type()
+        self._expect("", True)
+        while self._accept(","):
+            self._expect("", True)
+        self._expect(";")
 
     '''Compiles a sequence of statements, not including the enclosing ()'''
     def compile_statements(self):
-        NotImplemented
+        while self.tokenizer.token_type() == TokenType.keyword:
+            if self.tokenizer.keyword() == 'do':
+                self.compile_do()
+            elif self.tokenizer.keyword() == 'let':
+                self.compile_let()
+            elif self.tokenizer.keyword() == 'while':
+                self.compile_while()
+            elif self.tokenizer.keyword() == 'if':
+                self.compile_if()
+            elif self.tokenizer.keyword() == 'return':
+                self.compile_return
 
     def compile_do(self):
-        NotImplemented
+        self._expect("do")
+        self._expect("", True)
+        if self._accept("("): # go straight to expression list route
+            self.compile_expression_list()
+            self._expect(")")
+        else:
+            self._expect("", True) # foo.bar then expression list route
+            self._expect(".")
+            self._expect("", True) 
+            self._expect("(")
+            self.compile_expression_list()
+            self._expect(")")
 
     def compile_let(self):
         NotImplemented
